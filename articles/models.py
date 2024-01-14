369a956347112d0bd6d51ea6f0a5a743428c8a9e
paolovg95 <paolo9517@gmail.com>
2024-01-14 15:51:16 -0300

    test.py added and added some fields to Article Model class

--- articles/models.py
+++ articles/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Article(models.Model):
     title = models.TextField()
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(null=True,blank=True,auto_now_add=False, auto_now=False)
---
+++ try_django_inventory/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.contrib.auth.password_validation import validate_password
+import os
+
+class TreyDjangoSettingsTest(TestCase):
+
+    def test_secret_key(self):
+        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY2')
+        try:
+            is_valid = validate_password(SECRET_KEY) # Raise an exception if password too common
+        except Exception as e:
+            self.fail(e)
