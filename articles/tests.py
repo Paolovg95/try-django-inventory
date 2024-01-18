af06a9cc1ff9ddde0b6b4510124a7ddfe503f6e7
paolovg95 <paolo9517@gmail.com>
2024-01-18 00:55:02 -0300

    Tests added to articles app

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
-
+from .models import Article
 # Create your tests here.
+class ArticleModelTests(TestCase):
+    def setUp(self):
+        self.number_of_articles = 5
+        for i in range(0,self.number_of_articles):
+            Article.objects.create(title="Hello World", content="Something")
+
+
+    def test_articles_number(self):
+        qs = Article.objects.all()
+        self.assertEqual(qs.count(), self.number_of_articles)
+
+    def test_first_slug(self):
+        qs = Article.objects.first()
+        slug = qs.slug
+        self.assertTrue(slug, "hello-world")
+
+    def test_unique_slug(self):
+        qs = Article.objects.exclude(slug__iexact="hello-world")
+        for obj in qs:
+            slug = obj.slug
+            self.assertNotEqual(slug, "hello-world")
+
+    def test_slugify_instance_method(self):
+
+        qs = Article.objects.values_list("slug",flat=True)
+        slug_list = list(set(qs))
+        self.assertEqual(len(qs), len(slug_list))
--- try_django_inventory/tests.py
+++ try_django_inventory/tests.py
@@ -5,7 +5,7 @@ import os
 class TreyDjangoSettingsTest(TestCase):
 
     def test_secret_key(self):
-        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY2')
+        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
         try:
             is_valid = validate_password(SECRET_KEY) # Raise an exception if password too common
         except Exception as e:
