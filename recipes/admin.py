76b0f92240f4f982a8ae94f844d442969d5088a0
paolovg95 <paolo9517@gmail.com>
2024-01-20 20:09:11 -0300

    Admin panel set up using inlines for Recipes App

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Recipe, RecipeIngredient
 # Register your models here.
+
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    fields = ['name', 'quantity', 'unit', 'directions']
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ('name', 'description', 'id')
+    raw_id_fields = ('user', )
