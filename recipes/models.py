839292d98d1d9df4cd43c665dc049cf9fa887d74
paolovg95 <paolo9517@gmail.com>
2024-01-20 18:36:10 -0300

    Recipe app added

---
+++ recipes/models.py
@@ -0,0 +1,36 @@
+
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+# Create your models here.
+"""
+Global
+    - Ingredients
+    - Recipes
+User
+    - Recipes
+        - Ingredients
+
+"""
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    directions = models.TextField(max_length=300, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    quantity = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+    directions = models.TextField(max_length=300, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
